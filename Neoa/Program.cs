76a2676a2e7c465257d58048acdb7209fb9983db
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-01 22:30:18 +0000

    update

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ using System;
+
+ namespace Neoa
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
