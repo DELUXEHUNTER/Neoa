abd47913d07cc1626437ea9fd6f8584057c4ee5d
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-01 23:20:32 +0000

    commit

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -1,16 +1,1 @@
-﻿ using System;
-
- namespace Neoa
-{
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Game game = new Game();
-
-            game.Run();
-        }
-    }
-
-}
+﻿
