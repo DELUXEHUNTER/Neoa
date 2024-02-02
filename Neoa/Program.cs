db3bb8cf588912c7cf333a57c8077f0cb4843498
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-02 10:19:32 -0500

    commit

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -10,10 +10,32 @@ namespace Neoa
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
+            Console.WriteLine("      πππ       πππ    πππππππππππ       πππππππ           πππ            ");
+            Console.WriteLine("      ππππ      πππ    πππππππππππ     ππππ  πππππ        πππππ           ");
+            Console.WriteLine("      πππππ     πππ    πππ            πππ       πππ      πππ πππ          ");
+            Console.WriteLine("      ππππππ    πππ    πππ           πππ         πππ     πππ πππ          ");
+            Console.WriteLine("      πππ ππππ  πππ    ππππππππππ   πππ          πππ    πππ   πππ         ");
+            Console.WriteLine("      πππ  ππππ πππ    ππππππππππ   πππ          πππ   πππ    ππππ        ");
+            Console.WriteLine("      πππ    ππ πππ    πππ           πππ         πππ   πππππππππππ        ");
+            Console.WriteLine("      πππ    ππππππ    πππ           ππππ       ππππ  πππ      ππππ       ");
+            Console.WriteLine("      πππ     πππππ    πππ            ππππ    πππππ   πππ       πππ       ");
+            Console.WriteLine("      πππ       πππ    πππππππππππ      πππππππππ    πππ         πππ      ");
+
+
+
+
+
+
+
+
+
+
+
+
             string playername = Console.ReadLine();
 
             Console.WriteLine("Hello " + playername);
+            Console.ReadLine();
         }
     }
 }
