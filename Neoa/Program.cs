4a618f036b5a1f155039298595fa73147ca8e034
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-02 18:09:03 -0500

    commit

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -8,21 +8,38 @@ namespace Neoa
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("      πππ       πππ    πππππππππππ       πππππππ           πππ            ");
-            Console.WriteLine("      ππππ      πππ    πππππππππππ     ππππ  πππππ        πππππ           ");
-            Console.WriteLine("      πππππ     πππ    πππ            πππ       πππ      πππ πππ          ");
-            Console.WriteLine("      ππππππ    πππ    πππ           πππ         πππ     πππ πππ          ");
-            Console.WriteLine("      πππ ππππ  πππ    ππππππππππ   πππ          πππ    πππ   πππ         ");
-            Console.WriteLine("      πππ  ππππ πππ    ππππππππππ   πππ          πππ   πππ    ππππ        ");
-            Console.WriteLine("      πππ    ππ πππ    πππ           πππ         πππ   πππππππππππ        ");
-            Console.WriteLine("      πππ    ππππππ    πππ           ππππ       ππππ  πππ      ππππ       ");
-            Console.WriteLine("      πππ     πππππ    πππ            ππππ    πππππ   πππ       πππ       ");
-            Console.WriteLine("      πππ       πππ    πππππππππππ      πππππππππ    πππ         πππ      ");
-            Console.WriteLine();
-            Console.ReadLine(Start);
+        //Start Screen;
+         
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("         πππ       πππ    πππππππππππ       πππππππ             ");
+            Console.WriteLine("         ππππ      πππ    πππππππππππ     ππππ  πππππ           ");
+            Console.WriteLine("         πππππ     πππ    πππ            πππ       πππ          ");
+            Console.WriteLine("         ππππππ    πππ    πππ           πππ         πππ         ");
+            Console.WriteLine("         πππ ππππ  πππ    ππππππππππ   πππ          πππ         ");
+            Console.WriteLine("         πππ  ππππ πππ    ππππππππππ   πππ          πππ         ");
+            Console.WriteLine("         πππ    ππ πππ    πππ           πππ         πππ         ");
+            Console.WriteLine("         πππ    ππππππ    πππ           ππππ       ππππ         ");
+            Console.WriteLine("         πππ     πππππ    πππ            ππππ    πππππ          ");
+            Console.WriteLine("         πππ       πππ    πππππππππππ      πππππππππ            ");
+            Console.WriteLine("Welcome to the planet of Neo");
+            Console.WriteLine("    Are you ready for the adventure of your life?");
+            Console.WriteLine("                   To begin, start with your first name");
+            
+            //Naming.
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Now, for your last name");
+            Console.WriteLine("Note: Add a space before typing the last name, or it could end up like this example. ConnorKeon Connor = First name Keon = Last");
+
+
+            string lastname = Console.ReadLine();
+
+            Console.ResetColor();
+
+            Console.Clear();
         
+           
 
 
 
@@ -34,10 +51,31 @@ namespace Neoa
 
 
 
-            string playername = Console.ReadLine();
 
-            Console.WriteLine("Hello " + playername );
+
+
+
+
+            Console.WriteLine(firstname +  lastname );
+            Console.WriteLine("");
             Console.ReadLine();
         }
     }
 }
+
+
+/* NEOA  TITLE SCREEN
+
+            Console.WriteLine("         πππ       πππ    πππππππππππ       πππππππ           πππ               ");
+            Console.WriteLine("         ππππ      πππ    πππππππππππ     ππππ  πππππ        πππππ              ");
+            Console.WriteLine("         πππππ     πππ    πππ            πππ       πππ      πππ πππ             ");
+            Console.WriteLine("         ππππππ    πππ    πππ           πππ         πππ     πππ πππ             ");
+            Console.WriteLine("         πππ ππππ  πππ    ππππππππππ   πππ          πππ    πππ   πππ            ");
+            Console.WriteLine("         πππ  ππππ πππ    ππππππππππ   πππ          πππ   πππ    ππππ           ");
+            Console.WriteLine("         πππ    ππ πππ    πππ           πππ         πππ   πππππππππππ           ");
+            Console.WriteLine("         πππ    ππππππ    πππ           ππππ       ππππ  πππ      ππππ          ");
+            Console.WriteLine("         πππ     πππππ    πππ            ππππ    πππππ   πππ       πππ          ");
+            Console.WriteLine("         πππ       πππ    πππππππππππ      πππππππππ    πππ         πππ         ");
+  
+*/
+            
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Neo
 Welcome to Neo a simple C# game that I am going to be making completely online so it may not function properly but I will try my best to make a good game that someone will find and enjoy to play or even help me make. It might not be good but I'm learning and this is my first game of ANY type of code. Hopefully when its done you will enjoy it.
 
+###To-Do/Things I need help with
+* Literally everything
+* Slow-print Animation
+* Lore writing having an actual good idea for lore while sticking with my vision.
+* Lore Writing to actually make most of the game
+* Health, combat, ect...
+* Save system.
 
-## Lore
+
+
+### Short Lore
+This game takes place on the planet of Neo, a 
