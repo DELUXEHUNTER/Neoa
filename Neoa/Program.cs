8ff5153340510b95c8caac28dcdae0fb3263727c
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-02 08:39:45 -0500

    commit

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -1,2 +1,19 @@
-﻿using System 
-using
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Neoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string playername = Console.ReadLine();
+
+            Console.WriteLine("Hello" + playername) 
+        }
+    }
+}
