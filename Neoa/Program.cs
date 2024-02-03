99136d43b576ed4d23369a923c3f99a0605ae729
Deacon, Hunter <100684231+DELUXEHUNTER@users.noreply.github.com>
2024-02-02 19:05:54 -0500

    c

--- Neoa/Program.cs
+++ Neoa/Program.cs
@@ -23,21 +23,42 @@ namespace Neoa
             Console.WriteLine("         πππ    ππππππ    πππ           ππππ       ππππ         ");
             Console.WriteLine("         πππ     πππππ    πππ            ππππ    πππππ          ");
             Console.WriteLine("         πππ       πππ    πππππππππππ      πππππππππ            ");
-            Console.WriteLine("Welcome to the planet of Neo");
-            Console.WriteLine("    Are you ready for the adventure of your life?");
-            Console.WriteLine("                      To begin, start with your first name");
-            
+            Console.WriteLine(".... System initializing");
+            Console.WriteLine("       ......... Initialization complete");
+            Console.WriteLine("                      Enter operator first name.");
+
             //Naming.
             string firstname = Console.ReadLine();
-            Console.WriteLine("Now, for your last name");
-            Console.WriteLine("Note:Press spacebar once before typing the last name");
+            Console.WriteLine("last name.. ");
+            Console.WriteLine("... input required");
 
             string lastname = Console.ReadLine();
 
             Console.ResetColor();
 
             Console.Clear();
-        
+
+            //strings
+            string Gender; 
+            string Ethnicity;
+            string Occupation; 
+
+            do {
+                Console.Clear();
+                Console.WriteLine("Operator.. Gender input")
+                Console.WriteLine("Male, Female.")
+                Gender = Console.Readline().ToUpper ();
+                if (Gender == "MALE")
+                {
+                    correct = 1;
+                }
+                if (Gender == "FEMALE")
+                {
+                    correct == 1;
+                }
+                else {Console.WriteLine("Operator input invalid... retry input"); Console.ReadLine();}
+            } while(correct == 0);
+
            
 
 
@@ -55,7 +76,7 @@ namespace Neoa
 
 
 
-            Console.WriteLine("firstname + lastname );
+            Console.WriteLine(firstname + lastname );
             Console.WriteLine("");
             Console.ReadLine();
         }
--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Neo
-Welcome to Neo a simple C# game that I am going to be making completely online so it may not function properly but I will try my best to make a good game that someone will find and enjoy to play or even help me make. It might not be good but I'm learning and this is my first game of ANY type of code. Hopefully when its done you will enjoy it.
+Welcome to Neo, a simple C# game that I am going to be making completely online, so it may not function properly, but I will try my best to make a good game that someone will find and enjoy playing or even help me make. It might not be good, but I'm learning, and this is my first game of any type of code. Hopefully, when it's done, you will enjoy it. Check the To-Do list to see the features that need to be added. Anyone is welcome to contribute to the game as long as they do not ruin the game for others.
 
-###To-Do/Things I need help with
+
+##How to begin
+download the files, and then name yourself, then select all of the other options for your character, name should match the gender you have chosen, make sure to press space one time before entering last name. There are several different species to chose from but it is recommended you start out as a human.
+
+
+
+
+
+###To-Do/Things I need help with:
 * Literally everything
 * Slow-print Animation
 * Lore writing having an actual good idea for lore while sticking with my vision.
@@ -10,6 +18,9 @@ Welcome to Neo a simple C# game that I am going to be making completely online s
 * Save system.
 
 
+###Future Plans:
+In the future I plan to add more genders, backstories, ect... for now I don't have many plans or ideas until the game functions 
 
-### Short Lore
+
+### Short Lore:
 This game takes place on the planet of Neo, a 
